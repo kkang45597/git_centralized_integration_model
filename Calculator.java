1f9535292917a7f2e1bd3c8e0b6f286ef5510ef2
kkang4557 <kkang45597@gmail.com>
2025-07-24 14:52:44 +0900

    performOperation develop

--- Calculator.java
+++ Calculator.java
@@ -10,7 +10,17 @@ public class Calculator {
 			case '-':
 				result = num1 - num2;
 				break;
-			
+			case '*':
+				result = num1 * num2;
+				break;
+			case '/':
+				if (num2 != 0) {
+					result = num1 / num2;
+				} 
+				else {
+					System.out.println("0으로 나눌 수 없습니다.");
+                }
+                break;
             default:
                 System.out.println("올바른 연산자를 입력하세요.");
 		}
